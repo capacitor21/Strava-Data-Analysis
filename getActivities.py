8ef59853567944e04378f904b017ef6338d878a7
Michael DeDreu <mdedreu21@gmail.com>
2020-12-17 21:03:13 -0500

    Got polyline from each activity and plotted on folium map

--- getActivities.py
+++ getActivities.py
@@ -5,36 +5,38 @@ import time
 #url for when you need new code
 newTokenUrl = "http://www.strava.com/oauth/authorize?client_id=[clientID]&response_type=code&redirect_uri=http://localhost/exchange_token&approval_prompt=force&scope=profile:read_all,activity:read_all"
 
+#Makes request for latest activity data with new token if expired
+def getLastest():
 
-with open('strava_tokens.json') as tokenFile:
-    strava_tokens = json.load(tokenFile)
-with open('myInfo.json') as myInfo:
-    myInfo = json.load(myInfo)
+    #load token files
+    with open('strava_tokens.json') as tokenFile:
+        strava_tokens = json.load(tokenFile)
+    with open('myInfo.json') as myInfo:
+        myInfo = json.load(myInfo)
 
-#If current token has expired make post request for new one
-if strava_tokens['expires_at'] < time.time():
-    res = requests.post(
-        url = 'https://www.strava.com/oauth/token',
-        data = {
-            'client_id': myInfo['client_id'],
-            'client_secret': myInfo['client_secret'],
-            'code': myInfo['code'],
-            'grant_type': 'refresh_token',
-            'refresh_token': strava_tokens['refresh_token']
-        }
-    )
+    #If current token has expired make post request for new one
+    if strava_tokens['expires_at'] < time.time():
+        res = requests.post(
+            url = 'https://www.strava.com/oauth/token',
+            data = {
+                'client_id': myInfo['client_id'],
+                'client_secret': myInfo['client_secret'],
+                'grant_type': 'refresh_token',
+                'refresh_token': strava_tokens['refresh_token']
+            }
+        )
 
-    newtokens = res.json()
-    #update token file
-    with open('strava_tokens.json', 'w') as newStravaTokens:
-        json.dump(newtokens, newStravaTokens)
-    strava_tokens = newtokens
+        newtokens = res.json()
+        #update token file
+        with open('strava_tokens.json', 'w') as newStravaTokens:
+            json.dump(newtokens, newStravaTokens)
+        strava_tokens = newtokens
 
+    #Get activities
+    url = "https://www.strava.com/api/v3/activities?access_token="
+    res = requests.get(url + strava_tokens['access_token'] + "&per_page=200")
+    data = res.json()
 
-url = "https://www.strava.com/api/v3/activities?access_token="
-res = requests.get(url + strava_tokens['access_token'])
-data = res.json()
-
-#Write actives json to my activities json file
-with open('myActivities.json', 'w') as activitiesFile:
-    json.dump(data, activitiesFile)
+    #Write activities json to my activities json file
+    with open('myActivities.json', 'w') as activitiesFile:
+        json.dump(data, activitiesFile)
--- processData.py
+++ processData.py
@@ -1,3 +1,36 @@
 import json
 import pandas as pd
+import os
+import time
+import getActivities
+import folium
+from folium import plugins
+import decoder
 
+#if file doesn't exist make requests for new activities
+
+
+#getActivities.getLastest()
+#else if file hasn't been updated in a day
+    #make new request
+if not os.path.exists("myActivities.json"):
+    getActivities.getLastest()
+elif os.path.getctime("myActivities.json"):
+    print() 
+
+with open("myActivities.json") as activities:
+    data = json.load(activities)
+
+
+map = folium.Map(location=[40.8923, -74.4774], zoom_start=10) #intialize the map
+
+
+lines = []
+#Get all the polyline routes for each activity and decode them 
+for activity in data:
+    encodedLine = activity['map']['summary_polyline']
+    if encodedLine != None:
+        decoded = decoder.decode_polyline(activity['map']['summary_polyline']) #decode the line
+        folium.PolyLine(decoded).add_to(map) #add the line to the mao
+
+map.save('./map.html')
